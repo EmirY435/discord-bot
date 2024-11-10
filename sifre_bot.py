370d89ffad911ccd4b67a63fbcfbe6e7f84a41ac
EmirY435 <emir.yuceturk2012@gmail.com>
2024-11-10 21:19:07 +0300

    Update sifre_bot.py

--- sifre_bot.py
+++ sifre_bot.py
@@ -1,32 +1,27 @@
 import discord
-import random
-from bot_mantik import gen_pass
+from discord.ext import commands
 
-# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
 intents = discord.Intents.default()
-# Mesajları okuma ayrıcalığını etkinleştirelim
 intents.message_content = True
-# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} olarak giriş yaptık.')
+    print(f'{bot.user} olarak giriş yaptık')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$merhaba'):
-        await message.channel.send("Selam!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$How are u?'):
-        await message.channel.send("Thanks for asking, I am alright")
-    
-    elif message.content.startswith('$sifre'):
-        await message.channel.send(gen_pass(5))
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
 
-client.run("token")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+    import discord
+
+bot.run("token")
